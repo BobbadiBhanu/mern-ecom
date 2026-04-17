0c579ad8eda7dc998e6bdd49d2b824ac48c93288
Bobbadi Bhanu <bobbadibhanu1729@gmail.com>
2026-04-17 19:48:26 +0530

    Added the backend server

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 const API = axios.create({
-    baseURL:"https://mern-ecom-rbay.onrender.com/"
+    baseURL:"https://mern-ecom1-8nd8.onrender.com/"
 })
 API.interceptors.request.use((req)=>{
     const token=localStorage.getItem("token")
